1345a168b19f3fdd936bd2802a02edd9008a0d97
donna6355 <donnajun1214@gmail.com>
2024-07-12 18:02:36 +0900

    [add] enum basic

---
+++ variables/src/enum.rs
@@ -0,0 +1,23 @@
+enum IpAddr{
+    V4(u8,u8,u8,u8),
+    V6(String)
+}
+
+enum Message{
+    Quit,
+    Move {x:i32, y:i32},
+    Write(String),
+    ChangeColor(i32,i32,i32)
+}
+
+impl Message {
+    fn call(&self){
+        //method
+    }
+}
+fun enum_main(){
+    let home = IpAddr::V4(127,0,0,1);
+    let loopBack = IpAddr::V6(String::from("::1"));
+    let m = Message::Write(String::from("hello"));
+    m.call();
+}
