87d7fda09d3dfa45508a899c9eed001faeaa7968
donna6355 <donnajun1214@gmail.com>
2024-07-17 17:43:10 +0900

    [fix] module works

--- module/target/debug/deps/module-659795248d50b90e.d
+++ module/target/debug/deps/module-659795248d50b90e.d
@@ -1,6 +1,7 @@
-/Users/donna/Desktop/rust/module/target/debug/deps/module-659795248d50b90e: src/main.rs src/garden.rs
+/Users/donna/Desktop/rust/module/target/debug/deps/module-659795248d50b90e: src/main.rs src/garden.rs src/garden/vege.rs
 
-/Users/donna/Desktop/rust/module/target/debug/deps/module-659795248d50b90e.d: src/main.rs src/garden.rs
+/Users/donna/Desktop/rust/module/target/debug/deps/module-659795248d50b90e.d: src/main.rs src/garden.rs src/garden/vege.rs
 
 src/main.rs:
 src/garden.rs:
+src/garden/vege.rs:
---
+++ module/target/debug/module.d
@@ -0,0 +1,1 @@
+/Users/donna/Desktop/rust/module/target/debug/module: /Users/donna/Desktop/rust/module/src/garden/vege.rs /Users/donna/Desktop/rust/module/src/garden.rs /Users/donna/Desktop/rust/module/src/main.rs
