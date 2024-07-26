967217ac6a353e2c73efc6360b18f05bb937da75
donna6355 <donnajun1214@gmail.com>
2024-07-26 17:42:40 +0900

    [add] panic or Result

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,6 +2,24 @@ use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
 
+//create type to handle error
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value:i32) -> Guess {
+        if value<1 || value>100 {
+            panic!("Guess value must be between 1 and 100, got {value}");
+        }
+        Guess {value}
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number!");
 
@@ -18,10 +36,14 @@ fn main() {
             .expect("Failed to read line");
         println!("You guessed: {}", guess);
     
-        let guess: u32 = match guess.trim().parse() {
+        let guess: i32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
+        if(guess < 1 || guess > 100 ){
+            println!("The secret number will be between 1 and 100");
+            continue;
+        }
     
         match guess.cmp(&secret_number){
             Ordering::Less => println!("Too small!"),
